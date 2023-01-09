460b4ab0f5c6621403dcbf82331735ad7202a826
cpj509 <abzi110@naver.com>
2023-01-09 18:36:39 +0900

    syslog server test

---
+++ go-log-test/test-log/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/mcuadros/go-syslog.v2"
+)
+
+func main() {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+	server.ListenUDP("0.0.0.0:8514")
+	server.Boot()
+	log.Println("start log server")
+
+	go func(channel syslog.LogPartsChannel) {
+		for logParts := range channel {
+			fmt.Println(logParts)
+		}
+	}(channel)
+
+	server.Wait()
+}
