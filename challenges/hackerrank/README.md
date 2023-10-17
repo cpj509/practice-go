4d7c702c6174ff2e1c9e6d366b9ed296a10b830b
cpj509 <abzi110@naver.com>
2023-10-17 14:43:28 +0000

    simple array challenges

---
+++ challenges/hackerrank/README.md
@@ -0,0 +1,1 @@
+Copyright 2006-2023 HackerRank, Inc. All rights reserved.
---
+++ challenges/hackerrank/interview/array/2d/2d.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func hourglassSum(arr [][]int32) int32 {
+	var result int32 = -63
+	var sum int32 = 0
+
+	for x := 0; x < 4; x++ {
+		for y := 0; y < 4; y++ {
+			sum = arr[x][y] + arr[x][y+1] + arr[x][y+2] +
+				arr[x+1][y+1] +
+				arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2]
+			if sum > result {
+				result = sum
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	var arr [][]int32
+	for i := 0; i < 6; i++ {
+		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != 6 {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	result := hourglassSum(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
